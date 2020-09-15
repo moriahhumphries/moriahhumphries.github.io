3089781048a5a0026a946093e209ba904701f6dd
Moriah Humphries <moriah.humphries@gmail.com>
2020-09-15 14:00:24 -0500

    added background image, font, styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <h1>Future home of Moriah Humphries</h1>
+    <div className="App center-align" style={{"backgroundImage": ""}}>
+      <h1>The future home of Moriah Humphries.</h1>
 
     </div>
   );
