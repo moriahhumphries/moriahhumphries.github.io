1f1f5e76ee08d2cdcdf450d07a2a7529a537461d
Moriah Humphries <moriah.humphries@gmail.com>
2020-09-19 19:53:19 -0500

    added sections to skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -7,7 +7,7 @@ function Skills() {
             <h1>Skills</h1>
 
             <ul>
-                {/*<li style={{"fontSize": "40px", "textDecoration": "underline"}}>Front End</li>*/}
+                <li style={{"fontSize": "40px", "textDecoration": "underline"}}>Front End</li>
                 <li><i className="fab fa-html5"></i> HTML</li>
                 <li><i className="fab fa-css3-alt"></i> CSS</li>
                 <li><i className="fab fa-bootstrap"></i> BootStrap</li>
@@ -17,14 +17,14 @@ function Skills() {
                 <li><i className="fab fa-pagelines"></i> Thymeleaf</li>
                 {/*<br/>*/}
 
-                {/*<li style={{"fontSize": "40px", "textDecoration": "underline"}}>Back End</li>*/}
+                <li style={{"fontSize": "40px", "textDecoration": "underline"}}>Back End</li>
                 <li><i className="fab fa-java"></i> Java SE</li>
                 <li><i className="fab fa-java"></i> Java EE (Servlets, JSP)</li>
                 <li><i className="fas fa-leaf"></i> Spring Boot Framework</li>
                 <li><i className="fas fa-database"></i> MySQL</li>
                 {/*<br/>*/}
 
-                {/*<li style={{"fontSize": "40px", "textDecoration": "underline"}}>General</li>*/}
+                <li style={{"fontSize": "40px", "textDecoration": "underline"}}>General</li>
                 <li><i className="far fa-object-group"></i> Object-Oriented Programming</li>
                 <li><i className="fas fa-vial"></i> Test-Driven Development</li>
                 <li><i className="fas fa-user-friends"></i> Paired Programming</li>
